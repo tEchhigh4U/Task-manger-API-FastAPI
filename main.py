f80e7d52008ad6c55f89f79fa39a3beb80d8df87
William Hui <Williamhui@William-Hui.local>
2023-11-21 19:27:47 +0800

    update create route

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
+import mysql.connector
+from mysql.connector import Error
 
+# Establish a connection to the MySQL database
+def create_connection():
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            database="task-manager",
+            user="root",
+            password="root"
+        )
+        print("Connection to MySQL DB successful")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+    return connection
+
+# Create a connection to the database
+connection = create_connection()
 
 app = FastAPI()
 
@@ -11,13 +30,6 @@ class Task(BaseModel):
     description: str
     status: str
 
-# Sample data - replace with your own data storage mechanism
-tasks = [
-    {"id": 1, "title": "Task 1", "description": "Description 1", "status": "todo"},
-    {"id": 2, "title": "Task 2", "description": "Description 2", "status": "in progress"},
-    {"id": 3, "title": "Task 3", "description": "Description 3", "status": "done"},
-]
-
 # get all tasks
 @app.get("/tasks")
 def get_all_tasks():
@@ -26,17 +38,47 @@ def get_all_tasks():
 # create a task
 @app.post("/tasks")
 def create_task(task: Task):
-    result = {"title": Task.title, "description": Task.description, "status": Task.status}
-    return result
+    try:
+        # Create a cursor object to execute SQL queries
+        cursor = connection.cursor()
+
+        # Insert a new task
+        insert_query = 
+        "INSERT INTO tasks (title, description, status) VALUES (%s, %s, %s)"
+        task_data = (task.title, task.description, task.status)
+        cursor.execute(insert_query, task_data)
+
+        # Commit the changes to the database
+        connection.commit()
+
+        # Return the task
+        task_id = cursor.lastrowid
+
+        # Close the cursor and connection to the database
+        cursor.close()
+
+        # Return the task
+        result = {
+            "id": task_id,
+            "title": task.title,
+            "description": task.description,
+            "status": task.status
+        }
+        return result
+    except Error as e:
+        print(f"The error '{e}' occurred")
+        return {"Error": "Failed to create task"}
 
 # get a task by id
 @app.get("/tasks/{task_id}")
 def get_task(task_id: int, task: Task):
     if task_id == task.id:
-        result = {"task_id": task_id, "title": Task.title, 
-                    "description": Task.description, "status": Task.status}
+        result = {"task_id": task_id, 
+                  "title": Task.title, 
+                    "description": Task.description, 
+                    "status": Task.status}
         return result
-    return result
+    return {"message": "Task not found"}
 
 # delete a task by id
 @app.put("/tasks/{task_id}")
